936b7a2bd2ce922065a66c829e3e13782639f2e1
Mayank Trivedi <mayanktrivedi@Mayanks-MacBook-Air.local>
2026-07-10 17:07:30 +0530

    add

--- server/controller/interview.controller.js
+++ server/controller/interview.controller.js
@@ -4,7 +4,7 @@ import * as pdfjsLib from "pdfjs-dist/legacy/build/pdf.mjs"
 import { askAi } from "../services/openRouter.servics.js"
 import User from "../models/user.model.js"
 import Interview from "../models/interview.model.js"
-import Payment from "../models/payment.model.js"
+import Payment from "../models/Payment.model.js"
 export const analyzeResume = async (req, res) => {
     try {
         if (!req.file) {
